f2e0a14c9fd2d752a4d77dd16ce648d68929044e
ChrisEski <eschristos1@gmail.com>
2022-02-17 16:37:33 +0200

    Display colors in SingleColorPalette

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -3,44 +3,6 @@ import Navbar from "./Navbar";
 import ColorBox from "./ColorBox";
 import "../Styles/Palette.css";
 
-// class Palette extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { level: 500, format: "hex" };
-//     this.changeLevel = this.changeLevel.bind(this);
-//     this.changeFormat = this.changeFormat.bind(this);
-//   }
-
-//   changeLevel(level) {
-//     this.setState({ level });
-//   }
-
-//   changeFormat(value) {
-//     this.setState({ format: value });
-//   }
-
-//   render() {
-//     const { colors } = this.props.palette;
-//     const { level, format } = this.state;
-
-//     const colorBoxes = colors[level].map(color => (
-//       <ColorBox background={color[format]} name={color.name} />
-//     ));
-
-//     return (
-//       <div className="Palette">
-//         <Navbar
-//           level={level}
-//           changeLevel={this.changeLevel}
-//           handleChange={this.changeFormat}
-//         />
-//         <div className="Palette-colors">{colorBoxes}</div>
-//         {/* TODO: Footer goes here */}
-//       </div>
-//     );
-//   }
-// }
-
 class Palette extends Component {
   constructor(props) {
     super(props);
@@ -65,6 +27,7 @@ class Palette extends Component {
         name={color.name}
         key={color.id}
         moreUrl={`/palette/${id}/${color.id}`}
+        showLink={true}
       />
     ));
 
