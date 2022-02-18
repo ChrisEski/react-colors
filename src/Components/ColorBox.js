c521ddc4a471414a31a2bf81461313fb4df95427
ChrisEski <eschristos1@gmail.com>
2022-02-18 18:55:26 +0200

    Finish refactor of ColorBox CSS

--- src/Components/ColorBox.js
+++ src/Components/ColorBox.js
@@ -7,15 +7,15 @@ import "../Styles/ColorBox.css";
 
 const styles = {
   ColorBox: {
-    width: "20%",
     height: props => (props.showingFullPalette ? "25%" : "50%"),
+    width: "20%",
     margin: "0 auto",
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
     marginBottom: "-4px",
     "&:hover button": {
-      opacity: "1",
+      opacity: 1,
       transition: "0.5s",
     },
   },
@@ -68,7 +68,71 @@ const styles = {
     lineHeight: "30px",
     textTransform: "uppercase",
     textDecoration: "none",
+    opacity: 0,
+  },
+
+  boxContent: {
+    position: "absolute",
+    padding: " 10px",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+  },
+
+  copyOverlay: {
     opacity: "0",
+    zIndex: "0",
+    transition: "transform 0.6s ease-in-out",
+    transform: "scale(0.1)",
+  },
+
+  showOverlay: {
+    opacity: 1,
+    transform: "scale(50)",
+    zIndex: 10,
+    position: "absolute",
+  },
+
+  copyMessage: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      textTransform: "uppercase",
+      marginBottom: "0",
+      padding: "1rem",
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "100",
+    },
+  },
+
+  showMessage: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.3s",
   },
 };
 
@@ -97,25 +161,59 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && "show"}`}
+            className={`${classes.copyOverlay} ${
+              copied && classes.showOverlay
+            }`}
           />
-          <div className={`copy-msg ${copied && "show"}`}>
-            <h1>Copied!</h1>
+
+          <div
+            className={`${classes.copyMessage} ${
+              copied && classes.showMessage
+            }`}>
+            <h1>copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
           </div>
           {showingFullPalette && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-              <span className={classes.seeMore}>More</span>
+              <span className={classes.seeMore}>MORE</span>
             </Link>
           )}
         </div>
       </CopyToClipboard>
+      // <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+      //   <div style={{ background }} className={classes.ColorBox}>
+      //     <div
+      //       style={{ background }}
+      //       className={`${classes.copyOverlay} ${
+      //         copied && classes.showOverlay
+      //       }`}
+      //     />
+      //     <div
+      //       className={`${classes.copyMessage} ${
+      //         copied && classes.showMessage
+      //       }`}>
+      //       <h1>Copied!</h1>
+      //       <p className={classes.copyText}>{background}</p>
+      //     </div>
+      //     <div className="copy-container">
+      //       <div className={classes.boxContent}>
+      //         <span className={classes.colorName}>{name}</span>
+      //       </div>
+      //       <button className={classes.copyButton}>Copy</button>
+      //     </div>
+      //     {showingFullPalette && (
+      //       <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+      //         <span className={classes.seeMore}>More</span>
+      //       </Link>
+      //     )}
+      //   </div>
+      // </CopyToClipboard>
     );
   }
 }
